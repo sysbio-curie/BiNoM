576217170f2a732de83a8771c530aa553ce991ea
ebonnet <ebonnet@529c41bb-3191-6a72-8c4e-89ace72e129d>
2012-06-05 15:12:54 +0000

    merging maps update
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@13354 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/fr/curie/BiNoM/pathways/test/TestMergingMapsCD.java
+++ binom/BiNoM2011/src/fr/curie/BiNoM/pathways/test/TestMergingMapsCD.java
@@ -18,14 +18,23 @@ public class TestMergingMapsCD {
 	public static void main(String[] args) {
 
 		//String fileName = "/bioinfo/users/ebonnet/Binom/mergeMaps/a1.xml";
-		String fileName = "/bioinfo/users/ebonnet/Binom/mergeMaps/mTOR_scratch_v31.xml";
+		String fileName = "/bioinfo/users/ebonnet/test.xml";
+		//String fileName = "/bioinfo/users/ebonnet/Binom/mergeMaps/mTOR_scratch_v31.xml";
 		SbmlDocument cd = CellDesigner.loadCellDesigner(fileName);
 		String text = cd.toString();
+		String prefix  = "rz_";
 		
 		Vector<String> ids = Utils.extractAllStringBetween(text, "id=\"", "\"");
 		for(int i=0;i<ids.size();i++)
-			System.out.println(ids.get(i)+" ");
+			if (!ids.get(i).equals("default") && !ids.get(i).startsWith("rb_")) {
+				System.out.println(i+" => "+ids.get(i));
+				text = Utils.replaceString(text, "\""+ids.get(i)+"\"", "\""+prefix+""+ids.get(i)+"\"");
+				text = Utils.replaceString(text, ">"+ids.get(i)+"<", ">"+prefix+""+ids.get(i)+"<");
+			}
 		
+		SbmlDocument res = CellDesigner.loadCellDesignerFromText(text);
+		
+		CellDesigner.saveCellDesigner(res, "/bioinfo/users/ebonnet/res.xml");
 		
 	}
 

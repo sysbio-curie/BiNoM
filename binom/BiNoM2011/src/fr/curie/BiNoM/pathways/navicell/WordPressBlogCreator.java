9754688deca230979d6f9e2c1d61c1b2185b69ef
spook <spook@529c41bb-3191-6a72-8c4e-89ace72e129d>
2012-08-20 15:35:22 +0000

    remove unused Import
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@13961 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/fr/curie/BiNoM/pathways/navicell/WordPressBlogCreator.java
+++ binom/BiNoM2011/src/fr/curie/BiNoM/pathways/navicell/WordPressBlogCreator.java
@@ -21,7 +21,6 @@ package fr.curie.BiNoM.pathways.navicell;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;

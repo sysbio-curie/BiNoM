dd062dae12b14d2855adf9f84f5981f00138f448
eviara <eviara@529c41bb-3191-6a72-8c4e-89ace72e129d>
2013-08-26 15:19:51 +0000

    First release.
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@17983 529c41bb-3191-6a72-8c4e-89ace72e129d

---
+++ binom/BiNoM2011/src/javascript_lib/overlay.js
@@ -0,0 +1,164 @@
+/**
+ * Eric Viara (Sysra), $Id$
+ *
+ * Copyright (C) 2013 Curie Institute, 26 rue d'Ulm, 75005 Paris, France
+ * 
+ * BiNoM Cytoscape Plugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * BiNoM Cytoscape plugin is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+//var MAX_SCREEN_WIDTH = 1920;
+//var MAX_SCREEN_HEIGHT = 1200;
+var MAX_SCREEN_WIDTH = 2500;
+var MAX_SCREEN_HEIGHT = 2000;
+var overlay;
+
+USGSOverlay.prototype = new google.maps.OverlayView();
+
+function overlay_init() {
+	overlay = new USGSOverlay(map);
+}
+
+function USGSOverlay(map) {
+	this.map_ = map;
+	this.div_ = null;
+	this.setMap(map);
+	this.arrpos = [];
+}
+
+USGSOverlay.prototype.onAdd = function() {
+
+	function simpleBindShim(thisArg, func) {
+		return function() { func.apply(thisArg); };
+	}
+
+//	google.maps.event.addListener(this.getMap(), 'bounds_changed', simpleBindShim(this, this.resize));
+	google.maps.event.addListener(this.getMap(), 'center_changed', simpleBindShim(this, this.draw));
+	// ....
+
+	this.setMap(this.map_);
+
+	var div = document.createElement('div');
+	div.style.borderStyle = 'none';
+	div.style.borderWidth = '0px';
+	div.style.width = '100%';
+	div.style.height = '100%';
+	div.style.position = 'absolute';
+
+	div.left = 0;
+	div.top = 0;
+	div.style.left = '0px';
+	div.style.top = '0px';
+
+	var draw_canvas = document.createElement('canvas');
+	draw_canvas.style.left = '0px';
+	draw_canvas.style.top = '0px';
+
+	draw_canvas.width = MAX_SCREEN_WIDTH;
+	draw_canvas.height = MAX_SCREEN_HEIGHT;
+	draw_canvas.style.width = draw_canvas.width + 'px';
+	draw_canvas.style.height = draw_canvas.height + 'px';
+
+	draw_canvas.style.position = 'absolute';
+	div.appendChild(draw_canvas);
+
+	this.div_ = div;
+	this.context = draw_canvas.getContext('2d');
+	this.context.save();
+
+	var panes = this.getPanes();
+	panes.overlayLayer.appendChild(div);
+
+	this.draw();
+}
+
+USGSOverlay.prototype.resize = function() {
+	var overlayProjection = this.getProjection();
+	var ne = overlayProjection.fromLatLngToDivPixel(this.map_.getBounds().getSouthWest());
+	var sw = overlayProjection.fromLatLngToDivPixel(this.map_.getBounds().getNorthEast());
+
+	var width = ne.x - sw.x;
+	var height = sw.y - ne.y;
+
+	var div = this.div_;
+	if (div.width != width || div.height != height) {
+		div.width = width;
+		div.height = height;
+		div.style.width = width + 'px';
+		div.style.height = height + 'px';
+	}
+
+	var left = sw.x;
+	var top = ne.y;
+	if (div.left != left || div.top != top) {
+		div.left = left;
+		div.top = top;
+		div.style.left = left + 'px';
+		div.style.top = top + 'px';
+	}
+}
+
+USGSOverlay.prototype.draw = function() {
+
+	this.resize();
+
+	this.context.clearRect(0, 0, MAX_SCREEN_WIDTH, MAX_SCREEN_HEIGHT);
+
+	var arrpos = this.arrpos;
+	if (arrpos.length) {
+		var div = this.div_;
+		var overlayProjection = this.getProjection();
+		var mapProjection = this.map_.getProjection();
+		var scale = Math.pow(2, this.map_.zoom);
+
+		//this.context.fillStyle = 'rgba(100, 30, 100, 1)';
+
+		//console.log("drawing " + arrpos.length + " points");
+		for (var nn = 0; nn < arrpos.length; ++nn) {
+			//var latlng = map.getProjection().fromPointToLatLng(arrpos[nn].p);
+			var latlng = mapProjection.fromPointToLatLng(arrpos[nn].p);
+			var pix = overlayProjection.fromLatLngToDivPixel(latlng);
+			
+			var mod3 = (nn % 3);
+			var size = mod3+2;
+			if (mod3 == 0) {
+				this.context.fillStyle = 'rgba(100, 30, 100, 1)';
+			} else if (mod3 == 1) {
+				this.context.fillStyle = 'rgba(200, 200, 50, 1)';
+			} else {
+				this.context.fillStyle = 'rgba(100, 200, 100, 1)';
+			}
+			//console.log("Drawing " + arrpos[nn].id + " " + arrpos[nn].p);
+			this.context.fillRect(pix.x-div.left, pix.y-div.top, size*scale, size*scale);
+		}
+		
+		var latlng0 = mapProjection.fromPointToLatLng(arrpos[0].p);
+		var pix0 = overlayProjection.fromLatLngToDivPixel(latlng0);
+		this.context.strokeStyle = 'orange';
+		this.context.lineWidth = 1;
+		for (var nn = 1; nn < arrpos.length; ++nn) {
+			var latlng = mapProjection.fromPointToLatLng(arrpos[nn].p);
+			var pix = overlayProjection.fromLatLngToDivPixel(latlng);
+			this.context.beginPath();
+			this.context.moveTo(pix0.x-div.left, pix0.y-div.top);
+			this.context.lineTo(pix.x-div.left, pix.y-div.top);
+			this.context.stroke();
+		}
+	}
+}
+
+USGSOverlay.prototype.onRemove = function() {
+	this.div_.parentNode.removeChild(this.div_);
+	this.div_ = null;
+}

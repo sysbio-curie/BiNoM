2b5ba5aef28135bf38fecd5aa3188f2c50a31b77
eviara <eviara@529c41bb-3191-6a72-8c4e-89ace72e129d>
2013-12-09 13:14:47 +0000

    Added navicell icons in source tree
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@19313 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/scripts/build_navicell_lib.sh
+++ binom/BiNoM2011/scripts/build_navicell_lib.sh
@@ -10,12 +10,19 @@ if [ $# != 1 ]; then echo "usage: $0 TARFILE"; exit 1; fi
 tarfile=$1
 
 dir=$(dirname $0)
+echo $dir | grep "^\." > /dev/null
+if [ $? = 0 ]; then dir=$(pwd)/$dir; fi
+
+echo dir: $dir
 
 tmpdir=/tmp/build_navicell_lib.$$
 trap "echo | rm -rf $tmpdir" 0 1 2 3
 
 tmplibdir=$tmpdir/lib
 mkdir -p $tmplibdir
+
+tmpicondir=$tmpdir/map_icons
+mkdir -p $tmpicondir
 
 cd $dir/../navicell_lib
 
@@ -24,6 +31,10 @@ find . -print | grep -v "\.svn" | cpio -pdm $tmplibdir
 cd $dir/../src/javascript_lib
 
 find . -follow -print | grep -v "\.svn" | cpio -pdm $tmplibdir
+
+cd $dir/../navicell_icons
+
+find . -print | grep -v "\.svn" | cpio -pdm $tmpicondir
 
 echo $tarfile | grep "gz$" > /dev/null
 
@@ -41,7 +52,7 @@ fi
 
 cd $tmpdir
 
-tar $opts $tarfile lib
+tar $opts $tarfile lib map_icons
 
 echo "navicell lib tarfile: " $tarfile
 

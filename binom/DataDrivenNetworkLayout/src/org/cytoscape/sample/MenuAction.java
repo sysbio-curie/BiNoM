27228bdecde43b0ff1d291b6d8b7fd3735666c5c
uczerwin <uczerwin@529c41bb-3191-6a72-8c4e-89ace72e129d>
2014-03-12 14:41:17 +0000

    access the values of the attributes for a row
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@20644 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/DataDrivenNetworkLayout/src/org/cytoscape/sample/MenuAction.java
+++ binom/DataDrivenNetworkLayout/src/org/cytoscape/sample/MenuAction.java
@@ -6,12 +6,19 @@ import org.cytoscape.application.swing.AbstractCyAction;
 import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyRow;
 import org.cytoscape.model.CyTable;
+import org.cytoscape.model.CyColumn;
 import org.cytoscape.io.write.* ;
+import org.cytoscape.io.CyFileFilter;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.presentation.property.BasicVisualLexicon;
-import org.cytoscape.io.CyFileFilter;
+import java.util.*;
+
 import java.awt.event.ActionEvent;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 
 public class MenuAction extends AbstractCyAction {
     private final CyAppAdapter adapter;
@@ -32,9 +39,21 @@ public class MenuAction extends AbstractCyAction {
         
         CyNetwork mynetwork = manager.getCurrentNetwork();
         CyTable mytable = mynetwork.getDefaultNodeTable();
-        
+      
         int count = mytable.getRowCount();
-        System.out.print(count);
+        
+        List<CyNode> nodes = mynetwork.getNodeList();
+        CyNode mynode = nodes.get(50);
+        CyRow row = mynetwork.getRow(mynode);
+        String name = mynetwork.getRow(mynode).get("name", String.class);
+        List<CyRow> mylistrows = mytable.getAllRows(); 
+        Collection<CyColumn> mycolumns = mytable.getColumns();
+        Map<String,Object> values = row.getAllValues();
+        //CyTable mycytable=row.getTable(); 
+        System.out.println(count);
+        System.out.println(mynode);
+        System.out.println(name);
+        System.out.println(values);
         for (CyNode node : network.getNodeList()) {
             if (network.getNeighborList(node, CyEdge.Type.ANY).isEmpty())
                 networkView.getNodeView(node).setVisualProperty(

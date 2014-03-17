381f35847231b5e2c8b6305fb41f31e7cce0f289
uczerwin <uczerwin@529c41bb-3191-6a72-8c4e-89ace72e129d>
2014-03-17 15:43:40 +0000

    matrix creation not working
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@20742 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/DataDrivenNetworkLayout/src/org/cytoscape/sample/MenuAction.java
+++ binom/DataDrivenNetworkLayout/src/org/cytoscape/sample/MenuAction.java
@@ -17,15 +17,18 @@ import org.cytoscape.view.presentation.property.BasicVisualLexicon;
 import java.util.*;
 import java.awt.event.ActionEvent;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@SuppressWarnings("serial")
 public class MenuAction extends AbstractCyAction {
     private final CyAppAdapter adapter;
 
     public MenuAction(CyAppAdapter adapter) {
-        super("DataDrivenLayout",
+        super("DDL",
             adapter.getCyApplicationManager(),
             "network",
             adapter.getCyNetworkViewManager());
@@ -33,40 +36,75 @@ public class MenuAction extends AbstractCyAction {
         setPreferredMenu("Layout");
     }
     
-    public void actionPerformed(ActionEvent e) {
-        final CyApplicationManager manager = adapter.getCyApplicationManager();
-        final CyNetworkView networkView = manager.getCurrentNetworkView();
-        final CyNetwork network = networkView.getModel();
-        
-        CyNetwork mynetwork = manager.getCurrentNetwork();
-        CyTable mytable = mynetwork.getDefaultNodeTable();
-      
-        int count = mytable.getRowCount();
-        
-        List<CyNode> nodes = mynetwork.getNodeList();
-        CyNode mynode = nodes.get(50);
-        CyRow row = mynetwork.getRow(mynode);
-        String name = mynetwork.getRow(mynode).get("name", String.class);
-        List<CyRow> mylistrows = mytable.getAllRows(); 
-        Collection<CyColumn> mycolumns = mytable.getColumns();
-        Map<String,Object> values = row.getAllValues();
-        Set<String> keys = values.keySet(); //all colon names
-      
-        
-        
-        System.out.println(count);
-        System.out.println(mynode);
-        System.out.println(name);
-        System.out.println(keys);
-        for (CyNode node : mynetwork.getNodeList()) {
-            //if (network.getNeighborList(node, CyEdge.Type.ANY).isEmpty())
-                //networkView.getNodeView(node).setVisualProperty(
-                    //BasicVisualLexicon.NODE_VISIBLE, false);
-        	String myname = mynetwork.getRow(node).get("name", String.class);
-        	System.out.println(myname);
-        }
+	public void actionPerformed(ActionEvent e) {
+		final CyApplicationManager manager = adapter.getCyApplicationManager();
+		final CyNetworkView networkView = manager.getCurrentNetworkView();
+		final CyNetwork network = networkView.getModel();
 
-     
+		CyNetwork mynetwork = manager.getCurrentNetwork();
+		CyTable mytable = mynetwork.getDefaultNodeTable();
+
+		int count = mytable.getRowCount();
+
+		List<CyNode> nodes = mynetwork.getNodeList();
+		CyNode mynode = nodes.get(50);
+		CyRow row = mynetwork.getRow(mynode);
+		String name = mynetwork.getRow(mynode).get("name", String.class);
+		List<CyRow> mylistrows = mytable.getAllRows(); 
+		Collection<CyColumn> mycolumns = mytable.getColumns();
+		Map<String,Object> values = row.getAllValues();
+		Set<String> keys = values.keySet(); //all colon names
+
+
+
+		System.out.println(count);
+		//System.out.println(mynode);
+		//System.out.println(name);
+		//System.out.println(keys);
+
+		Set<String> set_attributes = new HashSet<String>();
+		for (CyNode node : mynetwork.getNodeList()) {
+			//if (network.getNeighborList(node, CyEdge.Type.ANY).isEmpty())
+			//networkView.getNodeView(node).setVisualProperty(
+			//BasicVisualLexicon.NODE_VISIBLE, false);
+			for (String col: values.keySet()){
+				if (col.startsWith("EW")){
+					set_attributes.add(col);
+					//System.out.println(col);
+				}
+			}
+			//String myname = mynetwork.getRow(node).get("name", String.class);
+			//System.out.println(myname);
+		}
+		Object [ ] [ ] matrix = new Object [mytable.getRowCount()] [set_attributes.size()] ; 
+		Map<String,Integer> map_index = new HashMap<String,Integer>();
+		int ct = 1;
+		for (String att : set_attributes){
+			map_index.put(att,ct);
+			ct++;
+		}
+		//System.out.println(map_index);
+		//System.out.println(set_attributes.size());
+		
+	
+        int ct2=0;
+        for (CyNode node : mynetwork.getNodeList()) {
+            ct2++;
+            //System.out.println(ct2);
+        	for (String col: values.keySet()){
+        		if (map_index.containsKey(col)){
+        		int index2=map_index.get(col);
+        		//System.out.println(index2);
+        		Object value = mynetwork.getRow(node).getAllValues().get(col);
+        		if (value != null){
+        		System.out.println(ct2);
+        		System.out.println(index2);
+        		//matrix[ct2][index2]=value; 
+        		System.out.println(value);}}
+        	}
+        	
+        }
+        //System.out.println(matrix);
     }
     
 }

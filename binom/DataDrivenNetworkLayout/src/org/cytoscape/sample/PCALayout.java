7b30d9a36476506be2d337cd7c362b6a25a3ca98
zinovyev <zinovyev@529c41bb-3191-6a72-8c4e-89ace72e129d>
2014-05-13 10:07:50 +0000

    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@22104 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/DataDrivenNetworkLayout/src/org/cytoscape/sample/PCALayout.java
+++ binom/DataDrivenNetworkLayout/src/org/cytoscape/sample/PCALayout.java
@@ -1,8 +1,10 @@
 package org.cytoscape.sample;
+
 import java.util.Random;
 
 import vdaoengine.analysis.PCAMethod;
-import vdaoengine.analysis.PCAMethodFixedCenter;
+import vdaoengine.analysis.elmap.ElmapAlgorithm;
+import vdaoengine.analysis.elmap.ElmapProjection;
 import vdaoengine.data.VDataSet;
 import vdaoengine.data.VDataTable;
 import vdaoengine.data.io.VDatReadWrite;
@@ -14,8 +16,31 @@ public class PCALayout {
 	public float geneProjections[][] = null; // This is used to make the layout
 	public float attributeContributions[][] = null; // This is not needed for layout but just in case
 	public float explainedVariation[] = null;
-	public int pcNumber;
-	/*public static void main(String[] args) {
+	
+	public String settings = "#ALG8\n"+
+			"algtype = epoch\n\n"+
+			"grid.type = simplerectangular\n"+
+			"grid.size1 = 10\n"+
+			"grid.size2 = 10\n\n"+
+			"init.stretch1 = 1\n"+
+			"init.stretch2 = 1\n"+
+			"init.stretch3 = 1\n\n"+
+			"epoch.id = 1\n"+
+			"epoch.minimize = true\n"+
+			"epoch.ep = 1\n"+
+			"epoch.rp = 250\n"+
+			"epoch.eps = 0.01\n"+
+			"epoch.epsSLAU = 0.01\n"+
+			"epoch.numiter = 3\n"+
+			"epoch.numiterSLAU = 1000\n\n"+
+			"epoch.id = 2\n"+
+			"epoch.minimize = true\n"+
+			"epoch.numiter = 10\n"+
+			"epoch.ep = 0.001\n"+
+			"epoch.rp = 20\n"+
+			"epoch.extrapolate = 3\n\n";
+	
+	public static void main(String[] args) {
 		int numberOfPoints = 100; // 100 points
 		float matrix[][] = new float[numberOfPoints][10]; // 10 dimensions
 		Random r = new Random();
@@ -27,17 +52,21 @@ public class PCALayout {
 		matrix[15][0] = Float.NaN;		
 		matrix[51][5] = Float.NaN;				
 		
-		//PCALayout pca = new PCALayout();
-		//pca.makeDataSet(matrix);
+		PCALayout pca = new PCALayout();
+		pca.makeDataSet(matrix);
 		//pca.loadDataSet("c:/datas/test/iris1.txt");
-		//pca.computePCA();
-		
-		System.out.println("POINT\tX\tY");
-		for(int i=0;i<numberOfPoints;i++)
-			System.out.println(i+"\t"+pca.geneProjections[i][0]+"\t"+pca.geneProjections[i][1]);
+		//pca.dataset = VSimpleProcedures.SimplyPreparedDataset(VDatReadWrite.LoadFromVDatFile("c:/datas/test/wine.dat"),-1);
+		pca.computePCA();
 		System.out.println("Variance explained by PC1 = "+pca.explainedVariation[0]);
 		System.out.println("Variance explained by PC2 = "+pca.explainedVariation[1]);		
-	}*/
+		
+		pca.computeNonlinearPCALayout();
+		System.out.println("\nVariance explained by nonlinear pca = "+pca.explainedVariation[0]);
+		
+		System.out.println("POINT\tX\tY");
+		for(int i=0;i<pca.geneProjections.length;i++)
+			System.out.println(i+"\t"+pca.geneProjections[i][0]+"\t"+pca.geneProjections[i][1]);
+	}
 	
 	public void makeDataSet(float matrix[][]){
 		dataset = new VDataSet();
@@ -53,6 +82,8 @@ public class PCALayout {
 	// this can load a dataset from simple tab-delimited text file with numbers and a header
 	public void loadDataSet(String fn){
 		VDataTable vt = VDatReadWrite.LoadFromSimpleDatFile(fn, true, "\t");
+		for(int i=0;i<vt.colCount;i++)
+			vt.fieldTypes[i] = vt.NUMERICAL;
 		dataset = VSimpleProcedures.SimplyPreparedDataset(vt, -1);
 	}
 	
@@ -60,22 +91,38 @@ public class PCALayout {
 		
 		PCAMethod pca = new PCAMethod();
 		pca.setDataSet(dataset);
-		pca.calcBasis(pcNumber);
+		pca.calcBasis(2);
 
-		attributeContributions = new float[dataset.coordCount][pcNumber];
-		for(int j=0;j<pcNumber;j++) for(int i=0;i<dataset.coordCount;i++) attributeContributions[i][j] = (float)pca.getBasis().basis[j][i];
+		attributeContributions = new float[dataset.coordCount][2];
+		for(int j=0;j<2;j++) for(int i=0;i<dataset.coordCount;i++) attributeContributions[i][j] = (float)pca.getBasis().basis[j][i];
 		
 		VDataSet vdprojected = pca.getProjectedDataset();
 		geneProjections = vdprojected.massif;
 		
-		explainedVariation = new float[pcNumber];
+		explainedVariation = new float[2];
 		double disp[] = pca.calcDispersionsRelative(dataset.simpleStatistics.totalDispersion*dataset.simpleStatistics.totalDispersion);
-		for (int i=0; i<pcNumber;i++){
-		explainedVariation[i] = (float)disp[i];
-		}
+		explainedVariation[0] = (float)disp[0];
+		explainedVariation[1] = (float)disp[1];
+	}
 	
-	
-		
+	public void computeNonlinearPCALayout(){
+		  ElmapProjection elmap = new ElmapProjection();
+	      ElmapAlgorithm ela = new ElmapAlgorithm();
+	      ela.setData(dataset);
+	      ela.readIniFileStr(settings,8);
+	      ela.computeElasticGrid();
+	      
+	      //ela.grid.saveToFile("c:/datas/test/iris1.vem", "iris1");
+	      //ela.grid.saveToFile("c:/datas/test/wine.vem", "wine");
+	      
+	      elmap.setElmap(ela);
+	      VDataSet vd = elmap.getProjectedDataset();
+	      geneProjections = vd.massif;
+	      
+	      explainedVariation = new float[1];
+	      explainedVariation[0] = 1-elmap.calculateMSEToProjection(dataset)/(dataset.simpleStatistics.totalDispersion*dataset.simpleStatistics.totalDispersion);
+	      
 	}
 
 }
+	

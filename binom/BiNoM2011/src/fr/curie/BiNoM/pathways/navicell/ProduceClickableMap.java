19fd3a2de9241938f2fa5cc38625d08ea3da82ef
eviara <eviara@529c41bb-3191-6a72-8c4e-89ace72e129d>
2014-11-21 15:29:18 +0000

    Added server mode in NaviCell factory
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@24849 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/fr/curie/BiNoM/pathways/navicell/ProduceClickableMap.java
+++ binom/BiNoM2011/src/fr/curie/BiNoM/pathways/navicell/ProduceClickableMap.java
@@ -5515,7 +5515,8 @@ public class ProduceClickableMap
 				out.println("    if (command) {");
 				out.println("      nv_decode(command);");
 				out.println("    }");
-				out.println("    if (navicell.id) {");
+				out.println("    var mode = '<?php echo $_GET[\"mode\"]; ?>'\n");
+				out.println("    if (navicell.id || mode == \"server\") {");
 				out.println("      nv_server(window, navicell.id);");
 				out.println("    }");
 			}

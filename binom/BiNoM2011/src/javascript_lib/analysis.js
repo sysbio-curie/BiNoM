f7a71a61d9fff42757542a8ea5bfb15c7e7a47a6
eviara <eviara@529c41bb-3191-6a72-8c4e-89ace72e129d>
2015-03-31 10:23:06 +0000

    Modify NaviCell license
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@26694 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/javascript_lib/analysis.js
+++ binom/BiNoM2011/src/javascript_lib/analysis.js
@@ -3,12 +3,12 @@
  *
  * Copyright (C) 2013-2015 Curie Institute, 26 rue d'Ulm, 75005 Paris, France
  * 
- * BiNoM Cytoscape Plugin is free software; you can redistribute it and/or
+ * NaviCell is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
 
- * BiNoM Cytoscape plugin is distributed in the hope that it will be
+ * NaviCell is distributed in the hope that it will be
  * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.

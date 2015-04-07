f181c39ecf148cde087b05f59250a62295b7ddb0
eviara <eviara@529c41bb-3191-6a72-8c4e-89ace72e129d>
2015-04-07 16:31:52 +0000

    Major improvments of REST API documentation.
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@26765 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/rest_api/gen_rest_api.sh
+++ binom/BiNoM2011/src/rest_api/gen_rest_api.sh
@@ -3,7 +3,7 @@
 # gen_rest_api.sh
 #
 
-sed -e 's/#.*//' | awk '
+sed -e 's/#.*//' -e 's/ /\&nbsp;/g' $1 | awk '
 BEGIN {
   FS="\t";
   in_table = 0;
@@ -14,13 +14,17 @@ substr($1, 1, 1) == "@" {
     printf("</table>\n\n");
   }
   if ($2) {
-     printf("<h3>%s</h3>\n", $2);
+     printf("<h3>%s</h3>\n\n", $2);
   }
   has_subcommand = substr($1, 2, 1) == "!";
-  printf("<table border=\"1\" cellpadding=\"4\" cellspacing=\"0\" class=\"action-table\">\n");
-  printf("<tr><th>Description</th><th>Action Name</th>%s<th colspan=\"4\">Arguments</th></tr>\n", (has_subcommand ? "<th>Command</th>" : ""));
-  in_table = 1;
   max_args = has_subcommand ? substr($1, 3) : substr($1, 2);
+  printf("<table border=\"1\" cellpadding=\"5\" cellspacing=\"0\" class=\"table-contents\">\n");
+  printf("<tr class=\"table-header\"><th>Description</th><th>Action Name</th>%s", (has_subcommand ? "<th>Command Argument</th>" : ""));
+  if (max_args > 0) {  
+    printf("<th colspan=\"8\">Arguments</th>");
+  }
+  printf("</tr>\n");
+  in_table = 1;
   next;
 }
 
@@ -33,15 +37,33 @@ NF > 1 {
     start = 3;
   }
   arg_cnt = 0;
+  example_cnt = 0;
+  example = ""
   for (nn = start; nn <= NF; nn+=2) {
     nn_1 = nn+1;
-    printf("<td><span class=\"variable\">%s</span> <span class=\"command\">:%s</span></td>", $nn, $nn_1);
+    if ($nn == "@example") {
+      example = (example_cnt > 0 ? example "<br/>" : "") "data=@COMMAND {\"action\": \"" $2 "\", \"args\": [" (has_subcommand ? "\"" $3 ($nn_1 ? "\", " : "\"") : "") $nn_1 "]}";
+      example_cnt++;
+      continue;
+    }
+    if ($nn_1 == "flag") {
+        style = "command";
+        $nn_1 = "";
+    } else {
+        style =  "variable";
+        $nn_1 = ":" $nn_1;
+    }
+    
+    printf("<td><span class=\"%s\">%s</span>&nbsp;<span class=\"command\">%s</span></td>", style, $nn, $nn_1);
     arg_cnt++;
   }
   for (nn = arg_cnt; nn < max_args; nn++) {
     printf("<td style=\"text-align: center\">-</td>");
   }
   printf("</tr>\n");
+  if (example_cnt > 0) {
+     printf("<tr class=\"table-example\"><td style=\"font-size: small; text-align: right\">Example%s</td><td colspan=\"5\">%s</td></tr>\n", (example_cnt > 1 ? "s" : ""), example);
+  }
 }
 
 END {

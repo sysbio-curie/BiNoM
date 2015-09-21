eebb08a725ca104b2a4b46ddf541c528a8a5f1fd
ebonnet <ebonnet@529c41bb-3191-6a72-8c4e-89ace72e129d>
2015-09-21 14:33:15 +0000

    Update CD to GeneSpring converter
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@30482 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/fr/curie/BiNoM/pathways/CellDesignerToGeneSpringConverter.java
+++ binom/BiNoM2011/src/fr/curie/BiNoM/pathways/CellDesignerToGeneSpringConverter.java
@@ -1,7 +1,19 @@
 package fr.curie.BiNoM.pathways;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
+import org.biopax.paxtools.model.level3.DnaRegion;
+import org.biopax.paxtools.model.level3.DnaRegionReference;
+import org.biopax.paxtools.model.level3.ProteinReference;
+import org.biopax.paxtools.model.level3.PublicationXref;
+import org.biopax.paxtools.model.level3.RelationshipXref;
+import org.biopax.paxtools.model.level3.RnaRegionReference;
 import org.sbml.x2001.ns.celldesigner.CelldesignerAntisenseRNADocument;
 import org.sbml.x2001.ns.celldesigner.CelldesignerComplexSpeciesAliasDocument;
 import org.sbml.x2001.ns.celldesigner.CelldesignerGeneDocument;
@@ -9,9 +21,12 @@ import org.sbml.x2001.ns.celldesigner.CelldesignerProteinDocument;
 import org.sbml.x2001.ns.celldesigner.CelldesignerRNADocument;
 import org.sbml.x2001.ns.celldesigner.CelldesignerSpeciesAliasDocument;
 import org.sbml.x2001.ns.celldesigner.CelldesignerSpeciesDocument;
+import org.sbml.x2001.ns.celldesigner.CelldesignerSpeciesIdentityDocument;
 import org.sbml.x2001.ns.celldesigner.ReactionDocument;
 import org.sbml.x2001.ns.celldesigner.SbmlDocument;
 import org.sbml.x2001.ns.celldesigner.SpeciesDocument;
+
+import fr.curie.BiNoM.pathways.utils.Utils;
 
 public class CellDesignerToGeneSpringConverter {
 
@@ -60,6 +75,8 @@ public class CellDesignerToGeneSpringConverter {
 	 * Map of CellDesigner antisense rna IDs to antisense rna objects.
 	 */
 	private HashMap<String, CelldesignerAntisenseRNADocument.CelldesignerAntisenseRNA> asrnas;
+	
+	private HashMap<String, HashSet<String>> entityReferences;
 
 
 	public CellDesignerToGeneSpringConverter() {
@@ -73,6 +90,7 @@ public class CellDesignerToGeneSpringConverter {
 		genes = new HashMap<String, CelldesignerGeneDocument.CelldesignerGene>();
 		rnas = new HashMap<String, CelldesignerRNADocument.CelldesignerRNA>();
 		asrnas = new HashMap<String, CelldesignerAntisenseRNADocument.CelldesignerAntisenseRNA>();
+		entityReferences = new HashMap<String, HashSet<String>>();
 	
 	}
 
@@ -104,7 +122,6 @@ public class CellDesignerToGeneSpringConverter {
 	
 	public void convert() {
 		
-		System.out.println("INFO: starting CellDesigner to BioPAX conversion...");
 		getSpecies();
 		getIncludedSpecies();
 		getReactions();
@@ -115,36 +132,124 @@ public class CellDesignerToGeneSpringConverter {
 		getAntisenseRNAs();
 		
 		// print some stats about the CD map
-		System.out.println("Species: "+this.species.size());
-		System.out.println("Included species: "+this.includedSpecies.size());
-		System.out.println("Reactions: "+this.reactions.size());
-		System.out.println("Complexes: "+this.complexes.size());
-		System.out.println("Proteins: "+this.proteins.size());
-		System.out.println("Genes: "+this.genes.size());
-		System.out.println("RNA: "+this.rnas.size());
-		System.out.println("AS RNA: "+this.asrnas.size());
+//		System.out.println("Species: "+this.species.size());
+//		System.out.println("Included species: "+this.includedSpecies.size());
+//		System.out.println("Reactions: "+this.reactions.size());
+//		System.out.println("Complexes: "+this.complexes.size());
+//		System.out.println("Proteins: "+this.proteins.size());
+//		System.out.println("Genes: "+this.genes.size());
+//		System.out.println("RNA: "+this.rnas.size());
+//		System.out.println("AS RNA: "+this.asrnas.size());
 		
+		this.buildEntityReferences();
+		
+		/*
+		 * Process aliases
+		 */
 		int numberOfAliases = this.sbml.getSbml().getModel().getAnnotation().getCelldesignerListOfSpeciesAliases().getCelldesignerSpeciesAliasArray().length;
 		System.out.println("Number of aliases: "+numberOfAliases);
 	
 		int numberOfComplexAliases = sbml.getSbml().getModel().getAnnotation().getCelldesignerListOfComplexSpeciesAliases().getCelldesignerComplexSpeciesAliasArray().length;		
 		System.out.println("Number of complex aliases: "+numberOfComplexAliases);
 		
-		for(int i=0;i<numberOfAliases;i++){
+		
+		BufferedWriter coord_file = null;
+		BufferedWriter hugo_file = null;
+		try {
+			coord_file = new BufferedWriter(new FileWriter("/Users/eric/coord.txt"));
+			hugo_file = new BufferedWriter(new FileWriter("/Users/eric/hugo.txt"));
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		
+		for (int i=0;i<numberOfAliases;i++) {
 			
 			CelldesignerSpeciesAliasDocument.CelldesignerSpeciesAlias spa = sbml.getSbml().getModel().getAnnotation().getCelldesignerListOfSpeciesAliases().getCelldesignerSpeciesAliasArray(i);
-			final String species_id = spa.getSpecies();
+			String species_id = spa.getSpecies();
 			
 			//System.out.println(spa);
-			System.out.println(species_id);
 			
 			String spa_id = spa.getId();
-			int pos_x = (int) Math.round(Float.parseFloat(spa.getCelldesignerBounds().getX()));
-			int pos_y = (int) Math.round(Float.parseFloat(spa.getCelldesignerBounds().getY()));
+			int x1 = (int) Math.round(Float.parseFloat(spa.getCelldesignerBounds().getX()));
+			int y1 = (int) Math.round(Float.parseFloat(spa.getCelldesignerBounds().getY()));
+			
 			int width = (int) Math.round(Float.parseFloat(spa.getCelldesignerBounds().getW()));
 			int height = (int) Math.round(Float.parseFloat(spa.getCelldesignerBounds().getH()));
 			
+			int x2 = x1 + width;
+			int y2 = y1 + height;
 			
+			SpeciesDocument.Species spe = species.get(species_id);
+			CelldesignerSpeciesDocument.CelldesignerSpecies inc_spe = this.includedSpecies.get(species_id);
+			
+			String speciesName = "";
+			String cdClass = "";
+			CelldesignerSpeciesIdentityDocument.CelldesignerSpeciesIdentity si = null;
+			
+			if (spe != null) {
+				// regular species
+				cdClass = Utils.getValue(spe.getAnnotation().getCelldesignerSpeciesIdentity().getCelldesignerClass());
+				speciesName = cleanString(Utils.getValue(spe.getName()));
+				si = spe.getAnnotation().getCelldesignerSpeciesIdentity();
+			}
+			else if (inc_spe != null){
+				// included species
+				speciesName = CellDesignerToCytoscapeConverter.convertSpeciesToName(sbml,inc_spe.getId(),true,true);
+				speciesName = cleanString(speciesName);
+				si = inc_spe.getCelldesignerAnnotation().getCelldesignerSpeciesIdentity();
+				cdClass = Utils.getValue(si.getCelldesignerClass());
+			}
+			else {
+				//speciesName = "WARNING";
+			}
+			
+			String referenceId = null;
+			if (cdClass.equals("PROTEIN")) {
+				referenceId = Utils.getValue(si.getCelldesignerProteinReference());
+				if (entityReferences.get(referenceId) == null)
+					System.out.println("null :"+ referenceId);
+			}
+			else if (cdClass.equals("GENE")) {
+				referenceId = Utils.getValue(si.getCelldesignerGeneReference());
+			}
+			else if (cdClass.equals("RNA")) {
+				referenceId = Utils.getValue(si.getCelldesignerRnaReference());
+			}
+			else if (cdClass.equals("ANTISENSE_RNA")) {
+				referenceId = Utils.getValue(si.getCelldesignerAntisensernaReference());
+			}
+			
+			String hugo_string = "";
+			if (referenceId != null) {
+				HashSet<String> hugoList = entityReferences.get(referenceId);
+				if (hugoList != null)
+					for (String hg : hugoList) {
+						try {
+							hugo_file.write(spa_id + "\t" + hg + "\n");
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+			}
+			
+			// rect (118,161) (121,163)	-	c_s418 (AKT1@Nucleus)
+			String out = "rect("+x1+","+y1+") ("+x2+","+y2+")"+"\t"+"-\t"+spa_id + " (" + speciesName + ")";
+			//System.out.println(out);
+			try {
+				coord_file.write(out + "\n");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		try {
+			coord_file.close();
+			hugo_file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	
 	}
@@ -264,10 +369,97 @@ public class CellDesignerToGeneSpringConverter {
 	}
 
 	
-	
+	/**
+	 * Eliminates spaces, stars, dashes from the string 
+	 */
+	private String cleanString(String s){
+    	s = Utils.replaceString(s, "@", "_at_");
+    	s = Utils.replaceString(s, ":", "_");
+    	s = Utils.replaceString(s, "|", "_");
+        char c[] = s.toCharArray();
+        for(int i=0;i<c.length;i++){
+        	if((c[i]>='a')&&(c[i]<='z')) continue;
+        	if((c[i]>='A')&&(c[i]<='Z')) continue;
+        	if((c[i]>='0')&&(c[i]<='9')) continue;
+        	if(c[i]=='_') continue;
+        	c[i] = '_';
+        }
+        // string starting with numbers not allowed
+        s = new String(c);
+        if((c[0]>='0')&&(c[0]<='9'))
+        	s = "id_"+s;
+        // remove any trailing underscore character(s)
+        while (s.endsWith("_") == true)
+        	s = s.substring(0, s.length()-1); 
+    	return s;
+	}
 	
 
+	private void buildEntityReferences() {
+		
+		for (String id : proteins.keySet()) {
+			CelldesignerProteinDocument.CelldesignerProtein prot = proteins.get(id);
+			//String protName = cleanString(prot.getName().getStringValue());
 
+			if (prot.getCelldesignerNotes() != null) {
+				String notes = Utils.getValue(prot.getCelldesignerNotes()).trim();
+				extractHUGOReferences(notes, id);
+			}
+		}
+		
+		for (String id : genes.keySet()) {
+			CelldesignerGeneDocument.CelldesignerGene gene = genes.get(id);
+			//String geneName = cleanString(gene.getName());
+			
+			if (gene.getCelldesignerNotes() != null) {
+				String notes = Utils.getValue(gene.getCelldesignerNotes()).trim();
+				extractHUGOReferences(notes, id);
+			}
+		}
+		
+		for (String id : rnas.keySet()) {
+			CelldesignerRNADocument.CelldesignerRNA rna = rnas.get(id);
+			//String rnaName = cleanString(rna.getName());
+			
+			if (rna.getCelldesignerNotes() != null) {
+				String notes = Utils.getValue(rna.getCelldesignerNotes()).trim();
+				extractHUGOReferences(notes, id);
+			}
+		}
+		
+		for (String id : asrnas.keySet()) {
+			CelldesignerAntisenseRNADocument.CelldesignerAntisenseRNA arna = asrnas.get(id);
+			//String arnaName = cleanString(arna.getName());
+			
+			// add publications and HUGO gene names
+			if (arna.getCelldesignerNotes() != null) {
+				String notes = Utils.getValue(arna.getCelldesignerNotes()).trim();
+				extractHUGOReferences(notes, id);
+			}
+		}
+	}
+	
+	private void extractHUGOReferences(String comment, String id) {
+		
+		StringTokenizer st = new StringTokenizer(comment," :\r\n\t;.,");
+		HashSet<String> hugo_list = new HashSet<String>();
+		while (st.hasMoreTokens()){
+			String ss = st.nextToken();
+			if (ss.toLowerCase().equals("hugo")) {
+				if(st.hasMoreTokens()) {
+					String hugo_id = st.nextToken();
+					hugo_list.add(hugo_id);
+				}
+			}
+		}
+		
+		for (String hugo_id : hugo_list) {
+			if (entityReferences.get(id) == null)
+				entityReferences.put(id, new HashSet<String>());
+			
+			entityReferences.get(id).add(hugo_id);
+		}
+	}
 
 
 }

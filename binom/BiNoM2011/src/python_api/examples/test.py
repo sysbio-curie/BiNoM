9265d181a5131212d03b789ccde98fafb4634edd
ebonnet <ebonnet@529c41bb-3191-6a72-8c4e-89ace72e129d>
2015-12-11 09:34:51 +0000

    Python binding example update
    git-svn-id: https://svn.curie.fr/svn/U900/research/software-dvt/binom/trunk@31871 529c41bb-3191-6a72-8c4e-89ace72e129d

--- binom/BiNoM2011/src/python_api/examples/test.py
+++ binom/BiNoM2011/src/python_api/examples/test.py
@@ -3,9 +3,11 @@ from curie.navicell import NaviCell, Options
 # set the minimal options (url of the web server and map used) and create a NaviCell object
 options = Options()
 
+# NaviCell production server
 #options.proxy_url = 'https://navicell.curie.fr/cgi-bin/nv_proxy.php'
 #options.map_url = 'https://navicell.curie.fr/navicell/maps/cellcycle/master/index.php'
 
+# Local NaviCell server
 options.proxy_url = 'http://localhost/nv_proxy.php'
 options.map_url = 'http://localhost/navicell/maps/cellcycle/master/index.php'
 
@@ -18,9 +20,7 @@ filename = "DU145_data.txt"
 dat = nv.makeDataFromFile(filename)
 nv.importDatatables(dat, "DU145", "mRNA expression data")
 
-# select a datatable, select a group of samples and produce a heatmap
-nv.mapStainingEditorSelectDatatable('', 'DU145')
-nv.mapStainingEditorSelectSample('', 'data')
-nv.mapStainingEditorApply('')
-
-
+# select a datatable, select a sample and produce a heatmap
+nv.heatmapEditorSelectDatatable('','0','DU145')
+nv.heatmapEditorSelectSample('','0','data')
+nv.heatmapEditorApply('')

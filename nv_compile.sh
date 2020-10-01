2a4f5b431f91fe41493e1bf1a8dc24d673c9bf98
Vincent Noël <contact@vincent-noel.fr>
2020-10-01 17:54:57 +0200

    Added acsnprocedures to navicell jar

--- nv_compile.sh
+++ nv_compile.sh
@@ -18,7 +18,7 @@ mkdir -p $bindir
 
     classpath=$topdir/binomlibext.jar:$topdir/cd4.jar:$topdir/celldesigner.jar:$topdir/CellDesigner401Jars.jar:$topdir/csml.jar:$topdir/cytoscape270.jar:$topdir/giny.jar:$topdir/imgscalr-lib-3.1-javadoc.jar:$topdir/imgscalr-lib-3.1-sources.jar:$topdir/imgscalr-lib-3.1.jar:$topdir/jwordpress/commons-collections-3.2.jar:$topdir/jwordpress/commons-configuration-1.5.jar:$topdir/jwordpress/commons-lang-2.3.jar:$topdir/jwordpress/jwordpress-0.5.1-cli.jar:$topdir/jwordpress/jwordpress-0.5.1.jar:$topdir/jwordpress/jwordpress-0.5.jar:$topdir/jwordpress/xmlrpc-client-1.1.jar:$topdir/MathMLIO.jar:$topdir/paxtools-4.2.1-no-jena.jar:$topdir/sbml.jar:$topdir/transpath.jar:$topdir/VDAOEngine.jar:$topdir/xgmml.jar
 
-    javac -cp $classpath:.:../$bindir -d ../$bindir fr/curie/BiNoM/pathways/navicell/ProduceClickableMap.java
+    javac -cp $classpath:.:../$bindir -d ../$bindir fr/curie/BiNoM/pathways/navicell/ProduceClickableMap.java fr/curie/BiNoM/pathways/utils/acsn/ACSNProcedures.java
 )
 
 (
